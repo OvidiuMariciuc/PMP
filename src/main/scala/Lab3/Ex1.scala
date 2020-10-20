76a89389f674aa87040d6843e0c8b9c65dc31632
OvidiuMariciuc <ovidiumariciuc99@gmail.com>
2020-10-20 22:03:16 +0300

    Laborator3

--- src/main/scala/Lab3/Ex1.scala
+++ src/main/scala/Lab3/Ex1.scala
@@ -1,15 +1,30 @@
-package Lab3 
+import com.cra.figaro.algorithm.factored.VariableElimination
+import com.cra.figaro.language.{Flip, Universe}
+import com.cra.figaro.library.compound.CPD
 
-import com.cra.figaro.language._
-import com.cra.figaro.algorithm.sampling._
+object Covid {
+	Universe.createNew()
+	private val febra = Flip(0.06)
+	private val tuse = Flip(0.04)
+	private val covid = CPD(febra, tuse,
+		(false, false) -> Flip(0.0001),
+		(false, true) -> Flip(0.1),
+		(true, false) -> Flip(0.5),
+		(true, true) -> Flip(0.99))
 
-object Ex1 {
-	def main(args: Array[String]) {
-		val test = Constant("Test")
-
-		val algorithm = Importance(1000, test)
-		algorithm.start()
-		
-		println(algorithm.probability(test, "Test"))
+	def main(args: Array[String]): Unit = {
+		//covid.observe(true)
+		febra.observe(true);
+		tuse.observe(true);
+		val alg = VariableElimination(febra, tuse,covid)
+		alg.start()
+		/*
+    println("Probability of febra: " + alg.probability(febra,
+      true))
+		println("Probability of tuse: " + alg.probability(tuse,
+			true))
+			*/
+		println("Probability of covid: " + alg.probability(covid,
+			true))
+		alg.kill
 	}
-}
