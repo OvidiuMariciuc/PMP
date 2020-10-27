2ad11a9d8473e318b948c561629bf04eb6b2325c
OvidiuMariciuc <ovidiumariciuc99@gmail.com>
2020-10-27 20:47:50 +0200

    Laborator4

--- src/main/scala/Lab3/Ex1.scala
+++ src/main/scala/Lab3/Ex1.scala
@@ -1,5 +1,8 @@
+package Lab3
+
 import com.cra.figaro.algorithm.factored.VariableElimination
-import com.cra.figaro.language.{Flip, Universe}
+import com.cra.figaro.language._
+import com.cra.figaro.algorithm.sampling._
 import com.cra.figaro.library.compound.CPD
 
 object Covid {
@@ -28,3 +31,50 @@ object Covid {
 			true))
 		alg.kill
 	}
+
+}
+
+/*
+object Burglary {
+  Universe.createNew()
+  private val burglary = Flip(0.01)
+  private val earthquake = Flip(0.0001)
+  private val alarm = CPD(burglary, earthquake,
+    (false, false) -> Flip(0.001),
+    (false, true) -> Flip(0.1),
+    (true, false) -> Flip(0.9),
+    (true, true) -> Flip(0.99))
+  private val johnCalls = CPD(alarm,
+    false -> Flip(0.01),
+    true -> Flip(0.7))
+
+  def main(args: Array[String]): Unit = {
+    johnCalls.observe(true)
+    val alg = VariableElimination(burglary, earthquake)
+    alg.start()
+    println("Probability of burglary: " + alg.probability(burglary,
+      true))
+    alg.kill
+  }
+}
+*/
+
+
+/*object Ex1 {
+  def main(args: Array[String]) {
+    val test = Constant("Test")
+
+    val algorithm = Importance(1000, test)
+    algorithm.start()
+
+    println(algorithm.probability(test, "Test"))
+    val helloWorldElement = Constant("Hello world!")
+    val sampleHelloWorld = Importance(1000, helloWorldElement)
+    sampleHelloWorld.start()
+    println("Probability of Hello world:")
+    println(sampleHelloWorld.probability(helloWorldElement, "Hello world!"))
+    println("Probability of Goodbye world:")
+    println(sampleHelloWorld.probability(helloWorldElement, "Goodbye world!"))
+
+  }
+}*/
--- src/main/scala/Lab4/Ex1.scala
+++ src/main/scala/Lab4/Ex1.scala
@@ -1,15 +1,57 @@
-package Lab4 
+package Lab4
 
+import com.cra.figaro.algorithm.factored.VariableElimination
 import com.cra.figaro.language._
 import com.cra.figaro.algorithm.sampling._
+import com.cra.figaro.library.atomic.discrete.FromRange
+import com.cra.figaro.library.compound.^^
 
 object Ex1 {
-	def main(args: Array[String]) {
-		val test = Constant("Test")
+  def main(args: Array[String]) {
+    //Exercise 4 - afiseaza 0.05555555555555555
+    val die1 = FromRange(1, 7)
+    val die2 = FromRange(1, 7)
+    val total4 = Apply(die1, die2, (i1: Int, i2: Int) => i1 + i2)
+    println(VariableElimination.probability(total4, 11))
 
-		val algorithm = Importance(1000, test)
-		algorithm.start()
-		
-		println(algorithm.probability(test, "Test"))
-	}
+    //Exercise 5 - afiseaza 0.4
+    val total5 = Apply(die1, die2, (i1: Int, i2: Int) => i1 + i2)
+    total5.addCondition((i: Int) => i > 8)
+    println(VariableElimination.probability(die1, 6))
+
+    //Exercise 6 - afiseaza 0.004629629629629629
+    def doubles = {
+      val die1 = FromRange(1, 7)
+      val die2 = FromRange(1, 7)
+      die1 === die2
+    }
+
+    val jail = doubles && doubles && doubles
+    println(VariableElimination.probability(jail, true))
+
+    //Exercise 7
+    //Here’s a Figaro representation for this game:
+    val numberOfSides =
+    Select(0.2 -> 4, 0.2 -> 6, 0.2 -> 8, 0.2 -> 12, 0.2 -> 20)
+    val roll = Chain(numberOfSides, ((i: Int) => FromRange(1, i + 1)))
+
+    // Exercise 8
+    val numberOfSides1 =
+      Select(0.2 -> 4, 0.2 -> 6, 0.2 -> 8, 0.2 -> 12, 0.2 -> 20)
+    val roll1 = Chain(numberOfSides1, ((i: Int) => FromRange(1, i + 1)))
+    val numberOfSides2 =
+      Select(0.2 -> 4, 0.2 -> 6, 0.2 -> 8, 0.2 -> 12, 0.2 -> 20)
+    val roll2 = Chain(numberOfSides2, ((i: Int) => FromRange(1, i + 1)))
+
+    def stickinessConstraint(sidesPair: (Int, Int)) =
+      if (sidesPair._1 == sidesPair._2) 1.0 else 0.5
+
+    val pairOfSides = ^^(numberOfSides1, numberOfSides2)
+    pairOfSides.addConstraint(stickinessConstraint)
+    println(VariableElimination.probability(roll2, 7))
+    // prints 0.05166666666666667
+    roll1.observe(7)
+    println(VariableElimination.probability(roll2, 7))
+    // prints 0.09704301075268815
+  }
 }
